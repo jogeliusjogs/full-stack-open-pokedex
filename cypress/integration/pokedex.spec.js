99b45d13909c4d4551f64e5bc960f3cb03522649
Jokke <jokke.ryynanen@gmail.com>
2021-09-26 14:36:58 +0300

    add test, yet another try

--- cypress/integration/pokedex.spec.js
+++ cypress/integration/pokedex.spec.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Individual pokemon page link works', function() {
+    cy.get('.list-item-name').contains('rattata').click()
+    cy.get('.pokemon-name').contains('rattata')
+  })
 })
